3f02d39336c10e8a5ea50ecae6fc2e8bdd765631
Farrukhsmit11 <farrukhmoten35@gmail.com>
2025-10-04 00:56:41 +0500

    Deleted unnecessary file

--- src/components/popularproducts/PopularProducts.jsx
+++ src/components/popularproducts/PopularProducts.jsx
@@ -2,17 +2,9 @@ import { Card } from "antd"
 const { Meta } = Card;
 import "./PopularProducts.css"
 import Productlist from "./Productlist.json"
-import { useState } from "react";
 import CardsWrapper from "../../uiComponents/cardsWrapper/CardsWrapper";
 
 const PopularProducts = () => {
-
-    const [DataTosave, setDataTosave] = useState("false");
-
-    const handleInputChange = (e) => {
-        setDataTosave(e.target.value);
-    }
-
 
     return (
         <section className="section-padding">
