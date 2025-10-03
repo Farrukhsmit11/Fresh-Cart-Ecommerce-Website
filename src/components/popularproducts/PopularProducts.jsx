cd20c3690034b0851d64363aefda37920f1a21c0
Farrukhsmit11 <farrukhmoten35@gmail.com>
2025-10-03 18:13:40 +0500

    clean code

--- src/components/popularproducts/PopularProducts.jsx
+++ src/components/popularproducts/PopularProducts.jsx
@@ -17,50 +17,9 @@ const PopularProducts = () => {
     return (
         <section className="section-padding">
             <div className="container">
-
-
                 <CardsWrapper
                     data={Productlist}
                 />
-
-
-
-                {/* <div className="row"> */}
-                {/* <h1 className="product-header-title">Popular Products</h1> */}
-                {/* </div> */}
-
-
-                {/* <div className="cards-grid">
-                    {Productlist.map((product, index) => {
-                        return (
-                            <div className="products-cards">
-                                <Card
-                                    className="card-two"
-                                    key={index}
-                                >
-                                    <div className="cards-header">
-                                        <img className="product-image" src={product.imgSrc} />
-                                    </div>
-                                    <div className="text-content">
-                                        <p className="product-description">{product.description}</p>
-                                        <h1 className="product-card-title">{product.title}</h1>
-                                    </div>
-                                    <Flex gap="middle" vertical>
-                                        <Flex gap="middle">
-                                            <div className="rating-price">
-                                                <Rate defaultValue={3} className="cart-rateing" />
-                                                <p>{product.price.toFixed(2)}</p>
-                                            </div>
-                                            <div className="add-to-cart">
-                                                <Button className="add-to-cart-button"><PlusOutlined /> Add</Button>
-                                            </div>
-                                        </Flex>
-                                    </Flex>
-                                </Card>
-                            </div>
-                        )
-                    })}
-                </div> */}
             </div>
         </section>
 
